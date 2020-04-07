c5524ffd145b27bfb06992ecbfa8cddfbf45dda3
kangtyu1251 <60689820+kangtyu1251@users.noreply.github.com>
2020-04-07 18:28:58 +0900

    Add files via upload

---
+++ 0314_Day2/0314_Day2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0314_Day2
+//
+//  Created by 朴萬浩 on 2020/03/14.
+//  Copyright © 2020 朴萬浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
